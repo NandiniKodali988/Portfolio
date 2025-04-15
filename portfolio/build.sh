aa6399760af3b22d97017f35f60ca63a87abc221
NandiniKodali988 <nandinikodali5@outlook.com>
2025-04-14 23:20:24 -0400

    build file

---
+++ portfolio/build.sh
@@ -0,0 +1,9 @@
+# PUSH WEBSITE TO GU DOMAINS 
+printf 'Would you like to push to GU domains? (y/n)? '
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then 
+    rsync -alvr --delete _site/ nandinik@gtown3.reclaimhosting.com:/home/nandinik/public_html/portfolio/_site
+
+else
+    echo NOT PUSHING TO GU DOMAINS!
+fi
