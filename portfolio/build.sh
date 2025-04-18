59ffd6f3a41a31b5f4f1cf9d9a69fbaf17da319f
NandiniKodali988 <nandinikodali5@outlook.com>
2025-04-17 21:19:47 -0400

    images

--- portfolio/build.sh
+++ portfolio/build.sh
@@ -2,7 +2,7 @@
 printf 'Would you like to push to GU domains? (y/n)? '
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then 
-    rsync -alvr --delete _site/ nandinik@gtown3.reclaimhosting.com:/home/nandinik/public_html/portfolio/_site
+    rsync -alvr --delete my_website/ nandinik@gtown3.reclaimhosting.com:/home/nandinik/public_html/my_website
 
 else
     echo NOT PUSHING TO GU DOMAINS!
